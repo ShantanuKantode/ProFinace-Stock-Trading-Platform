d028f439566784410d285a21a7b70d19d638022d
Shantanu <shantanukantode2@gmail.com>
2026-09-17 17:40:42 +0530

    Create Schema and Model

---
+++ backend/model/HoldingsModel.js
@@ -0,0 +1,8 @@
+const {model} = require("mongoose");
+
+const {HoldingSchema, HoldingsSchema} = require("../schemas/HoldingsSchema");
+
+const HoldingsModel = new model("holding" , HoldingsSchema);
+
+module.exports = {HoldingsModel};
+
