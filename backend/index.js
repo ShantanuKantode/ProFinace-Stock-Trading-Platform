dc0b78429efaf8edab7cbc39a476a266757e968a
Shantanu <shantanukantode2@gmail.com>
2026-09-17 17:29:47 +0530

    MongoDB connect with Express

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,18 @@
+require('dotenv').config()
+
 const express = require("express");
+const mongoose  = require("mongoose");
+
+const PORT = process.env.PORT || 3002; 
+const uri =  process.env.MONGO_URI ;
+
+
 
 const app = express();
 
+
 app.listen(3002 , () =>{
    console.log("App started !");
+   mongoose.connect(uri);
+   console.log("Database Connected");
 });
