c38cc9c60e2c760e4e07c3451f9d1e49c538e5b7
Shantanu <shantanukantode2@gmail.com>
2026-09-17 16:33:31 +0530

    Dashboard Component Complety done

--- dashboard/src/components/Menu.jsx
+++ dashboard/src/components/Menu.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
+
 import { Link } from "react-router-dom";
 
 const Menu = () => {
   const [selectedMenu, setSelectedMenu] = useState(0);
+  const [isProfileDropdownOpen, setIsProfileDropdownOpen] = useState(false);
 
   const handleMenuClick = (index) => {
     setSelectedMenu(index);
+  };
+
+  const handleProfileClick = (index) => {
+    setIsProfileDropdownOpen(!isProfileDropdownOpen);
   };
 
   const menuClass = "menu";
@@ -13,90 +19,78 @@ const Menu = () => {
 
   return (
     <div className="menu-container">
-      <img
-        src="/logo.png"
-        alt="Logo"
-        className="logo"
-      />
-
+      <img src="logo.png" style={{ width: "50px" }} />
       <div className="menus">
         <ul>
           <li>
             <Link
-              to="/"
               style={{ textDecoration: "none" }}
+              to="/"
               onClick={() => handleMenuClick(0)}
             >
-              <p
-                className={
-                  selectedMenu === 0
-                    ? activeMenuClass
-                    : menuClass
-                }
-              >
+              <p className={selectedMenu === 0 ? activeMenuClass : menuClass}>
                 Dashboard
               </p>
             </Link>
           </li>
-
           <li>
             <Link
-              to="/holdings"
               style={{ textDecoration: "none" }}
+              to="/orders"
               onClick={() => handleMenuClick(1)}
             >
-              <p
-                className={
-                  selectedMenu === 1
-                    ? activeMenuClass
-                    : menuClass
-                }
-              >
+              <p className={selectedMenu === 1 ? activeMenuClass : menuClass}>
+                Orders
+              </p>
+            </Link>
+          </li>
+          <li>
+            <Link
+              style={{ textDecoration: "none" }}
+              to="/holdings"
+              onClick={() => handleMenuClick(2)}
+            >
+              <p className={selectedMenu === 2 ? activeMenuClass : menuClass}>
                 Holdings
               </p>
             </Link>
           </li>
-
           <li>
             <Link
-              to="/funds"
               style={{ textDecoration: "none" }}
-              onClick={() => handleMenuClick(2)}
+              to="/positions"
+              onClick={() => handleMenuClick(3)}
             >
-              <p
-                className={
-                  selectedMenu === 2
-                    ? activeMenuClass
-                    : menuClass
-                }
-              >
+              <p className={selectedMenu === 3 ? activeMenuClass : menuClass}>
+                Positions
+              </p>
+            </Link>
+          </li>
+          <li>
+            <Link
+              style={{ textDecoration: "none" }}
+              to="funds"
+              onClick={() => handleMenuClick(4)}
+            >
+              <p className={selectedMenu === 4 ? activeMenuClass : menuClass}>
                 Funds
               </p>
             </Link>
           </li>
-
           <li>
             <Link
-              to="/apps"
               style={{ textDecoration: "none" }}
-              onClick={() => handleMenuClick(3)}
+              to="/apps"
+              onClick={() => handleMenuClick(6)}
             >
-              <p
-                className={
-                  selectedMenu === 3
-                    ? activeMenuClass
-                    : menuClass
-                }
-              >
+              <p className={selectedMenu === 6 ? activeMenuClass : menuClass}>
                 Apps
               </p>
             </Link>
           </li>
         </ul>
-
         <hr />
-
-        <div className="profile">
+        <div className="profile" onClick={handleProfileClick}>
           <div className="avatar">ZU</div>
           <p className="username">USERID</p>
         </div>
